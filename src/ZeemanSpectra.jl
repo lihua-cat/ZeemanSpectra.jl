20a25e0fc9f1d0d93ea84c4b1415e1dad6369db4
Wemko <yinyangxiangsheng@gmail.com>
2021-10-16 23:04:08 +0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.20

---
+++ src/ZeemanSpectra.jl
@@ -0,0 +1,5 @@
+module ZeemanSpectra
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ZeemanSpectra
+using Test
+
+@testset "ZeemanSpectra.jl" begin
+    # Write your tests here.
+end
